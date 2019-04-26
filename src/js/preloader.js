cb7b8c16ce4e1cffd250c30404e259491c62822b
remzov <remzovnik@gmail.com>
2019-04-26 14:41:19 +0300

    preloader work

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,7 +1,15 @@
 (function() {
   var preloader = document.querySelector('.js-preloader');
-
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('content loaded');
+  });
   window.addEventListener('load', () => {
-    preloader.hidden = true;
+    setTimeout(() => {
+      preloader.classList.add('slide-out');
+    }, 3000);
+
+    preloader.addEventListener('animationend', () => {
+      preloader.hidden = true;
+    });
   });
 })();
